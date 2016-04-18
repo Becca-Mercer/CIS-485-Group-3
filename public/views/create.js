97c6ebfee4bc0aa6b1c3d5ecb2db612e5ca4c5d1
Rebecca Mercer <rebecca.mercer2017@gmail.com>
2016-04-18 16:29:20 -0400

    Create Presentation -> Homepage
    Left all other parts of Homepage as written except where i needed to
    make changes to make the create Presentation button work and be
    formatted correctly

--- public/views/create.js
+++ public/views/create.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 	$("#new_presentation").click(function(){
 		$("#presentation").modal('show');
-		console.log("new_presentation");
 	});
 	$("#new_slide").click(function(){
 		$("#presentation").modal('hide');
 		$("#slide").modal('show');
-		console.log("new_slide");
 	});
 	$("#slide_content").click(function(){
 		$("#slide").modal('hide');
 		$("#content").modal('show');
-		console.log("slide_content");
 	});
 	$("#next_slide").click(function(){
 		$("#slide").modal('show');
+		$("#content").modal('hide');
+	});
+	$("#close").click(function(){
+		$("presentation").modal('hide');
+		$("#slide").modal('hide');
 		$("#content").modal('hide');
 	});
 });
